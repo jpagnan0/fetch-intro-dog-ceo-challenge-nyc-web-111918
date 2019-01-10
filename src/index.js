ef2f3045ef10da7888f7b4bdce6dce95dc724905
jpagnan0 <a.jpagnano@gmail.com>
2019-01-10 16:54:04 -0500

    challenge 1,2,3 done TODO: Filter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function (e) {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+  const imageContainer = document.querySelector('#dog-image-container');
+  const dogBreeds = document.querySelector("#dog-breeds");
+
+  fetch(imgUrl)
+  .then(response => response.json())
+  .then((data) => {
+    return data.message.map((img) => {
+      return imageContainer.innerHTML += `<img src="${img}" />`
+    });
+  });
+
+  fetch(breedUrl)
+  .then(response => response.json())
+  .then((dogBreed) => {
+    // console.log(dogBreed.message)
+    return Object.keys(dogBreed.message).map((breed) => {
+      return dogBreeds.innerHTML += `<li><font color="black">${breed}</font></li>`
+    });
+  });
+
+  dogBreeds.addEventListener('click', (e) => {
+    // e.target.color = 'black'
+     console.log(e.target.color)
+
+    if(e.target.color === 'black')
+    {
+      e.target.color = 'red'
+      console.log(e.target.color)
+    }
+    else {
+      e.target.color = 'black'
+    }
+  });
+
+});
